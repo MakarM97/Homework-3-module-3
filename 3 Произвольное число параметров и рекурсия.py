cca11f9fbb7f9ae48801a72140aa84d59f2ce670
MakarM97 <165716716+MakarM97@users.noreply.github.com>
2024-04-19 12:38:20 +1000

    Add files via upload

--- 3 Произвольное число параметров и рекурсия.py
+++ 3 Произвольное число параметров и рекурсия.py
@@ -7,14 +7,15 @@ def test(txt, *params, **second):
 test('Что-то здесь', 12, 13, 14, name='Anton', second_name='Kapustin')
 
 
-def summa(n):
+def factorial(n):
     if n == 0:
-        return 0
+        return 1
     else:
-        return n + summa(n - 1)
+        return n * factorial(n - 1)
 
 
-print(summa(6))
+print(factorial(5))
+#Ответ верный получается, но сам не смог додуматься как написать функцию для факториала
 
 
 
